16ccd9bd128c53fb5d1969db2199b322f6f1712d
mdebrouwer <matt.debrouwer@gmail.com>
2015-04-05 09:37:38 +1000

    Added stringutil and other string examples

--- hello.go
+++ hello.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"unicode/utf8"
+	"github.com/mdebrouwer/stringutil"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	sample := "!oG ,olleH"
+	fmt.Println(stringutil.Reverse(sample))
+
+	for i := 0; i < len(sample); i++ {
+		fmt.Printf("%x ", sample[i])
+	}
+
+	const nihongo = "日本語"
+	
+	for index, runeValue := range nihongo {
+        fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
+    }
+
+    for i, w := 0, 0; i < len(nihongo); i += w {
+        runeValue, width := utf8.DecodeRuneInString(nihongo[i:])
+        fmt.Printf("%#U starts at byte position %d\n", runeValue, i)
+        w = width
+    }
 }
